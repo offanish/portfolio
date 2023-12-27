e339ff90bd37259b6c51cdb47bb0880239377214
Anish Gautam <76646115+offanish@users.noreply.github.com>
2023-12-27 15:20:09 +0545

    apostrophe fix

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4">
-      <h1 className="text-6xl mb-12">Hi! I'm Anish. Something amazing is coming... ;)</h1>
+      <h1 className="text-6xl mb-12">Hi! I&apos;m Anish. Something amazing is coming... ;)</h1>
     </main>
   )
 }
